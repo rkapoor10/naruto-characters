3dc9fd7f308c27f514b7a18a2a0a42bf92caf7e4
RITIK KAPOOR <61417822+rkapoor10@users.noreply.github.com>
2021-07-10 02:13:35 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import { anime } from "./Characters.js";
+
+import "./styles.css";
+var url =
+  "https://www.pngkey.com/png/full/143-1431670_download-naruto-logo-naruto-shippuden-logo-png.png";
+
+export default function App() {
+  var [catg, setCatg] = useState("team");
+  function onClickHandler(catg) {
+    setCatg(catg);
+    console.log(catg);
+  }
+  return (
+    <div className="App">
+      <div className="container">
+        <img id="logo" src={url} alt=" " />
+        <h3>Tap any character to know more about them</h3>
+        <hr />
+        <button onClick={() => onClickHandler("team")}>Team 7</button>
+        <button onClick={() => onClickHandler("akatsuki")}>Akatsuki</button>
+        <button onClick={() => onClickHandler("otsutsuki")}>Otsutsuki</button>
+        <div className="row">
+          {anime[catg].map((c) => {
+            return (
+              <div className="charContainer">
+                <img id="characters" src={c.image} alt=" " />
+                <p style={{ color: "white" }}>{c.name}</p>
+              </div>
+            );
+          })}
+        </div>
+        <div>{}</div>
+      </div>
+    </div>
+  );
+}
---
+++ src/Characters.js
@@ -0,0 +1,134 @@
+const anime = {
+  team: [
+    {
+      name: "Naruto",
+      image:
+        "https://prodigits.co.uk/thumbs/wallpapers/p2/anime/15/3c63b88312284802.jpg"
+    },
+    {
+      name: "Saskue",
+      image: "https://assets.puzzlefactory.pl/puzzle/310/355/original.jpg"
+    },
+    {
+      name: "Sakura",
+      image:
+        "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/32087293-2555-4709-b3da-02403abfbd80/dd0d5eu-6c98190e-7e33-49f3-841a-55d01b0aea25.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzMyMDg3MjkzLTI1NTUtNDcwOS1iM2RhLTAyNDAzYWJmYmQ4MFwvZGQwZDVldS02Yzk4MTkwZS03ZTMzLTQ5ZjMtODQxYS01NWQwMWIwYWVhMjUuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.LXaYskjLKe9trsiyEDV-HdkBuRTe9MqnncmyBbtHXS0"
+    },
+    {
+      name: "Sai",
+      image:
+        "https://preview.redd.it/bh07t3uvh5861.jpg?width=600&format=pjpg&auto=webp&s=c71eb6ad15a0f0103284aee52fb0597acb3cfdb1"
+    },
+    {
+      name: "Yamato",
+      image:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRFL3upgQTpMx_uP6vZyOLQ90QY-2zX2Sw78Q&usqp=CAU"
+    },
+    {
+      name: "Kakashi",
+      image:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSe3r2hLzyaz94ASm4dYOTANp-BHALg50BCQg&usqp=CAU"
+    }
+  ],
+  akatsuki: [
+    {
+      name: "Obito",
+      image:
+        "https://www.wallpapersun.com/wp-content/uploads/2020/12/Obito-Wallpaper-13.jpg"
+    },
+    {
+      name: "Itachi",
+      image:
+        "https://us02-imgcdn.ymcart.com/33322/2019/02/23/8/c/8ced844a048fe119.jpg?x-oss-process=image/quality,Q_90/auto-orient,1/resize,m_lfit,w_500,h_500"
+    },
+    {
+      name: "Orochimaru",
+      image:
+        "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/2d026fb8-e222-42e1-a5f4-e049075c34ab/d3e05hr-719c96c8-9992-4d44-97e2-e6656dcf241e.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzJkMDI2ZmI4LWUyMjItNDJlMS1hNWY0LWUwNDkwNzVjMzRhYlwvZDNlMDVoci03MTljOTZjOC05OTkyLTRkNDQtOTdlMi1lNjY1NmRjZjI0MWUucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.oOzS8mVfRs0Ve3V69ccXJZuH2VHlScNfl_ZQrawnkVM"
+    },
+    {
+      name: "Nagato",
+      image:
+        "https://www.pngkit.com/png/detail/328-3287269_nagato-akatsuki-png-naruto-nagato-png.png"
+    },
+    {
+      name: "Konan",
+      image: "https://img.wallpapersafari.com/tablet/768/1024/78/31/LR0Glz.jpg"
+    },
+    {
+      name: "Hidan",
+      image:
+        "https://i.pinimg.com/originals/ca/51/e9/ca51e9c45174316b504b307289139261.jpg"
+    },
+    {
+      name: "Deidara",
+      image:
+        "https://pm1.narvii.com/7789/42842cb2bf98f063e71862070ac13a9b62e9aac9r1-725-1102v2_hq.jpg"
+    },
+    {
+      name: "Kakuzu",
+      image:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTY9BpPZ0cpzOkhZ-xKjrvKJlKLtMv_Ky_5WQXR-k_DlmKZWahVRlfFbZ0EeO-MM-gwfF4&usqp=CAU"
+    },
+    {
+      name: "Sasori",
+      image:
+        "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/1325ad04-cf2e-40ff-ad8c-7c9950df54ea/d6l6ifg-7c347be9-d029-45f1-9b8f-33a65082a545.png/v1/fill/w_1024,h_1423,strp/akatsuki___sasori___color__by_stingcunha_d6l6ifg-fullview.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTQyMyIsInBhdGgiOiJcL2ZcLzEzMjVhZDA0LWNmMmUtNDBmZi1hZDhjLTdjOTk1MGRmNTRlYVwvZDZsNmlmZy03YzM0N2JlOS1kMDI5LTQ1ZjEtOWI4Zi0zM2E2NTA4MmE1NDUucG5nIiwid2lkdGgiOiI8PTEwMjQifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.9lC40wdkk35hj7IS3ztmHotcUckhJjT5Rcl_CauMPho"
+    },
+    {
+      name: "Kisame",
+      image:
+        "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/90a9d40d-63b0-442b-95ee-9fa9fa8f5a50/db6xikk-b17cb55b-f0c2-4028-a653-9e39f8c3cd51.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzkwYTlkNDBkLTYzYjAtNDQyYi05NWVlLTlmYTlmYThmNWE1MFwvZGI2eGlray1iMTdjYjU1Yi1mMGMyLTQwMjgtYTY1My05ZTM5ZjhjM2NkNTEucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.bHCkRmjTdsxZAsrI6F1DSRzTTDyQ6y9olFNtm0rpvIE"
+    },
+    {
+      name: "Zetsu",
+      image:
+        "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/90a9d40d-63b0-442b-95ee-9fa9fa8f5a50/db6xj6k-2f487244-ac8c-46bd-b329-a1d0ece97b85.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3sicGF0aCI6IlwvZlwvOTBhOWQ0MGQtNjNiMC00NDJiLTk1ZWUtOWZhOWZhOGY1YTUwXC9kYjZ4ajZrLTJmNDg3MjQ0LWFjOGMtNDZiZC1iMzI5LWExZDBlY2U5N2I4NS5wbmcifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6ZmlsZS5kb3dubG9hZCJdfQ.L-j6CnbgOEK4duAdqV-qQoxxDR9o-I9TuJjxYZWwPmk"
+    }
+  ],
+  otsutsuki: [
+    {
+      name: "Hagoromo Otsutsuki",
+      image:
+        "https://static.wikia.nocookie.net/xmenkids/images/4/41/HagoromoOtsutsuki2.jpg "
+    },
+    {
+      name: "Kaguya Otsutsuki",
+      image:
+        "https://pm1.narvii.com/7672/f75a190144e5eb5f2a63e11dbf3c94b576c5c490r1-640-1136v2_hq.jpg"
+    },
+    {
+      name: "Isshiki Otsutsuki",
+      image: "https://pbs.twimg.com/media/Ed7QKM-WoAAjg9v.jpg:large"
+    },
+    {
+      name: "Kinshiki Otsutsuki",
+      image:
+        "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/32b8f7d9-0d6c-4e2f-b737-eea02c841b00/ddp5pnh-85336b00-38ac-4289-af81-81f726381860.png/v1/fill/w_1024,h_1667,strp/momoshiki_otsutsuki__byakugan_hands__by_uchihaclanancestor_ddp5pnh-fullview.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTY2NyIsInBhdGgiOiJcL2ZcLzMyYjhmN2Q5LTBkNmMtNGUyZi1iNzM3LWVlYTAyYzg0MWIwMFwvZGRwNXBuaC04NTMzNmIwMC0zOGFjLTQyODktYWY4MS04MWY3MjYzODE4NjAucG5nIiwid2lkdGgiOiI8PTEwMjQifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.iyxsqlXzG2HvcTz4gxTEz-komFbmiYM1Nxno0Em7MFk"
+    },
+    {
+      name: "Momoshiki Otsutsuki",
+      image:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT_fRbxwTGE3CzYCjTHslnjPMpkIPZsNTP_Cejg7E20mxyqG4q_H0gJrkKcBZgefG3lGkw&usqp=CAU"
+    },
+    {
+      name: "Asura Otsutsuki",
+      image: "https://pbs.twimg.com/media/EWMk9wvVAAACq5U.jpg"
+    },
+    {
+      name: "Indra Otsutsuki",
+      image: "https://cdn130.picsart.com/273999971016201.jpg?r1024x1024"
+    },
+    {
+      name: "Urashiki Otsutsuki",
+      image:
+        "https://static.wikia.nocookie.net/villains/images/e/ec/Urashiki_Otsutsuki.png"
+    },
+    {
+      name: "Toneri Otsutsuki",
+      image: "https://pbs.twimg.com/media/B4HEXqAIIAA6PBp.jpg"
+    }
+  ]
+};
+
+export { anime };
